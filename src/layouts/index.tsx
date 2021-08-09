c2a44b6f3a5c154958451d3d1616131e951730b8
guoqun <guoqun@induschain.cn>
2021-08-09 18:53:38 +0800

    feat:全局布局

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Layout, Menu } from 'antd';
+const { Header, Content, Footer } = Layout;
 
-function BasicLayout(props:any) {
+import styles from './index.less';
+
+function BasicLayout(props: any) {
   return (
     <div>
-      <h1>Yay! Welcome to umi!</h1>
-      {props.children}
+      <Layout>
+        <Header>
+          <div className={styles.logo}>王者荣耀资料库</div>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">英雄</Menu.Item>
+            <Menu.Item key="2">局内道具</Menu.Item>
+            <Menu.Item key="3">召唤师技能</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+            {props.children}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Umi 入门</Footer>
+      </Layout>
     </div>
   );
 }
