07d6075572147e48547788f7c8504ef3f5e28ddf
onpine <718215627@qq.com>
2021-08-10 21:21:41 +0800

    feat:proxy 请求代理

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { ResponseError } from 'umi-request';
 
 export const request = {
-  prefix: 'https://pvp.qq.com',
+  prefix: '/api',
   errorHandler: (error: ResponseError) => {
     // 集中处理错误
     console.log(error);
